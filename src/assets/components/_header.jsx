d7ef8d28af096b86e66d6bfdc42b95bab5e10c01
FabioMijango <00156022@uca.edu.sv>
2024-11-04 21:58:51 -0600

    Fix Heros and contact me styles in big res

--- src/assets/components/_header.jsx
+++ src/assets/components/_header.jsx
@@ -9,8 +9,8 @@ function _header() {
             <button className="px-6 font-Inter font-light text-white">Button</button>{/*Change theme button*/}
          </nav>
 
-         <section className="p-16 flex items-center place-self-center"> {/* Section for the hero */}
-            <div className="w-1/2">
+         <section className="p-32 flex items-center justify-center place-self-center "> {/* Section for the hero */}
+            <div className="max-w-4xl">
                <p className="font-Inter_Light font-light text-white">Hey, I&apos;m</p>
                <h1 className="font-GTWalsheim text-8xl text-transparent bg-cover bg-clip-text bg-gradient-to-r from-gr-0 via-gr-55 to-gr-100 py-3">
                   Fabio Mijango
@@ -24,12 +24,12 @@ function _header() {
                   Sed nisi. Nulla quis sem at nibh elementum imperdiet.
                </p>
             </div>
-            <div className="w-1/2 h-96 bg-gray-500">
-            Photo
-            </div>
+            <div className="animate-pulse max-w-3xl w-96 h-96 bg-gray-700 rounded-lg place-self-center"/>
+
+            {/* </div> */}
          </section>
 
-         <section id="contact" className="place-self-start px-20 flex pb-16 gap-10"> {/* Section for contact me */}
+         <section id="contact" className="place-self-center px-20 flex pb-16 gap-10"> {/* Section for contact me */}
             <button className="max-w-xs w-auto rounded-lg p-1 relative bg-gradient-to-r from-gr-0 to-gr-30">
                <div className="text-white font-Inter font-light rounded-lg bg-secondary-darkgray p-5 items-center place-self-center hover:bg-transparent">
                   Send an email

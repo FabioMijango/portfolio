744dba09052abaac7c79c8d5d8c5bd4fec87e001
FabioMijango <00156022@uca.edu.sv>
2024-11-05 21:02:59 -0600

    Icons to export easy way

---
+++ src/assets/images/icons.js
@@ -0,0 +1,9 @@
+import { FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { SiReaddotcv } from "react-icons/si";
+
+export const icons = {
+   github: FaGithub,
+   email: MdEmail,
+   cv: SiReaddotcv,
+};
